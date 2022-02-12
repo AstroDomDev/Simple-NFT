0b45436ca020f909bb900eba6793ad39c41599f2
FakeBigBoi <nickernoose@gmail.com>
2022-02-12 15:48:24 -0500

    Ordering changes

--- simple-nft.py
+++ simple-nft.py
@@ -2,29 +2,39 @@ from PIL import Image
 import os
 import sys
 
+### Sort Dictionary Reverse Numerically ###
 def sorted_dict(dictionary):
     output_dict = dict()
     dict_keys = sorted(dictionary.keys())
+    for i in dict_keys:
+        output_dict[i] = dictionary[i]
+    return output_dict
+
+### Compress Dictionary Leaving No Gaps in Numbers ###
+def compressed_dict(dictionary):
+    keys = list(dictionary.keys())
+    for key in keys:
+        while key-1 not in list(dictionary.keys()) and key > 1: dictionary[key-1] = dictionary[key]; del dictionary[key]; key = key-1
+    return dictionary
+
+def reversed_dict(dictionary):
+    output_dict = dict()
+    dict_keys = list(dictionary.keys())
     dict_keys.reverse()
     for i in dict_keys:
         output_dict[i] = dictionary[i]
     return output_dict
 
-def compressed_dict(dictionary):
-    keys = list(dictionary.keys())
-    keys.reverse()
-    for key in keys:
-        while key-1 not in list(dictionary.keys()) and key > 0: dictionary[key-1] = dictionary[key]; del dictionary[key]; key = key-1
-    return dictionary
-
+### Set Order of Current Layer ###
 def set_order(layer, dictionary):
-    try: z_index = int(input(f"What index position would you like {layer} to be placed (0 = first layer, usually background): "))
+    try: z_index = int(input(f"What index position would you like {layer} to be placed (1 = first layer, usually background): "))
     except ValueError: print("Index value must be integer."); return set_order(layer, dictionary)
     except: print("Unexpected Error... restarting."); main()
     if z_index in list(dictionary.keys()): print("Index value is already taken."); return set_order(layer, dictionary)
-    elif z_index < 0: print("Index value must be non-negative."); return set_order(layer, dictionary)
+    elif z_index < 1: print("Index value must be positive."); return set_order(layer, dictionary)
     else: return int(z_index)
 
+### Confirm Layer Ordering ###
 def order_confirmation():
     restart = input("Is this order correct? y | n: ")
     if restart.lower().startswith('n'): return False
@@ -47,6 +57,7 @@ def main():
     ### Sort Order ###
     order = sorted_dict(order)
     order = compressed_dict(order)
+    order = reversed_dict(order)
 
     ### Confirm Order ###
     for i in list(order.keys()):
